0d67e9448edf51f61768e1e6a4b9701a7bbb00e6
coderaiser <mnemonic.enemy@gmail.com>
2023-01-17 11:11:30 +0200

    chore: update

--- menu/copy-url-to-current-file.js
+++ menu/copy-url-to-current-file.js
@@ -1,9 +1,11 @@
+export const copyURLToCurrentFile = async ({DOM}) => {
+    const {path} = DOM.CurrentInfo;
+    const url = `${window.location.href}${path}`;
+    const {default: clipboard}  = await import('https://cdn.skypack.dev/@cloudcmd/clipboard');
+
+    await clipboard.writeText(url); 
+};
+
 export default {
-    'F6 - Copy URL to Current File': async ({DOM}) => {
-        const {path} = DOM.CurrentInfo;
-        const url = `${window.location.href}${path}`;
-        const {default: clipboard}  = await import('https://cdn.skypack.dev/@cloudcmd/clipboard');
-        
-        await clipboard.writeText(url); 
-    }
+    'F6 - Copy URL to Current File': copyURLToCurrentFile,
 }
